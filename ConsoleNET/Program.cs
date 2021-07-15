ce067ad11489e7fbe29982e9f00a003ef52a7aa0
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-15 13:37:11 +0900

    定数の宣言

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -6,12 +6,8 @@ class MyApp
     {
         //変数：値の再代入が可能
         //定数：値の再代入が不可能
-
-        // 変数の型　変数名;　と書いて変数を宣言する
-        //string msg;
-
-        //　変数を宣言すると同時に値を代入もできる
-        string msg = "Hello World";
+        //　定数の定義は const 定数の型 定数の名 =  "値";
+        const string msg = "Hello World";
         Console.WriteLine(msg);
     }
 }
