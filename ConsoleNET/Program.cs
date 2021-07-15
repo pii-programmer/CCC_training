0b5bab033eb4ba1526d5f7a7e74584b52754e41e
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-15 13:33:05 +0900

    変数の型と名を宣言する

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -4,10 +4,14 @@ class MyApp
 {
     static void Main()
     {
-       //一行コメント
-       /*
-        複数行コメント
-       */
-        Console.WriteLine("Hello World");
+        //変数：値の再代入が可能
+        //定数：値の再代入が不可能
+
+        // 変数の型　変数名;　と書いて変数を宣言する
+        //string msg;
+
+        //　変数を宣言すると同時に値を代入もできる
+        string msg = "Hello World";
+        Console.WriteLine(msg);
     }
 }
