025e2bc748bdf30619deeab5ae427f6c16e709f9
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-22 08:10:24 +0900

    switch条件分岐

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -4,10 +4,24 @@ class MyApp
 {
     static void Main()
     {
-        //まず関数 score を定義する。
-        var score = int.Parse(Console.ReadLine());
-        //if文の省略した書き方
-        Console.WriteLine((score > 80) ? "Great" : "so so...");
-        //Console.WriteLine((条件式)? "条件式がTrueならA" ："FalseならBと表示する");
+        //信号機を条件分岐文で書いてみる。今回ユーザーからの入力は文字列なので、メソッドは不要、この書き方でOK。
+        var signal = Console.ReadLine();
+        //switchによる条件分岐文はこちら
+        switch (signal)
+        {
+            case "red":
+                Console.WriteLine("Stop!");
+                break;
+            case "blue":
+            case "green": // or の時は、case を書き足す。
+                Console.WriteLine("Go!");
+                break;
+            case "yellow":
+                Console.WriteLine("Caution!");
+                break;
+            default:     // else の時は、default と書く。
+                Console.WriteLine("wrong signal!");
+                break;
+        }
     }
-}
+} 
