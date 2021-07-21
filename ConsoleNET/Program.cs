593e791e110fa83d72dbab29158df7af2ac10306
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-22 08:15:48 +0900

    while文の基本

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -4,24 +4,14 @@ class MyApp
 {
     static void Main()
     {
-        //信号機を条件分岐文で書いてみる。今回ユーザーからの入力は文字列なので、メソッドは不要、この書き方でOK。
-        var signal = Console.ReadLine();
-        //switchによる条件分岐文はこちら
-        switch (signal)
+        // each文＝ while文
+        //ある条件が続く間、この処理を繰り返します。というのが while(ある条件){この処理}文
+        var i = 0;
+
+        while (i < 10)
         {
-            case "red":
-                Console.WriteLine("Stop!");
-                break;
-            case "blue":
-            case "green": // or の時は、case を書き足す。
-                Console.WriteLine("Go!");
-                break;
-            case "yellow":
-                Console.WriteLine("Caution!");
-                break;
-            default:     // else の時は、default と書く。
-                Console.WriteLine("wrong signal!");
-                break;
+            Console.WriteLine(i);
+            i++;
         }
     }
 } 
