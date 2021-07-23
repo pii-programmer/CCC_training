9eeb040210024435bfb37868ecbda6573f476256
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-23 15:19:58 +0900

    for文のbreak

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -5,12 +5,12 @@ class MyApp
     static void Main()
     {
         //for文
-        // continue その条件だけ処理を中止して次のループへ
-        // 結果は「0 1 2 3 4 6 7 8 9」
+        // break
+        // 結果は「0 1 2 3 4」
         for (var i = 0; i < 10; i++) {
             if (i == 5)
             {
-                continue;
+                break;
             }
             Console.WriteLine(i);
         }
