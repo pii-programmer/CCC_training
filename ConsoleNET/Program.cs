bcde1b1cd90dc1d6ee9f9336e53200e49bab26e0
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-24 17:04:10 +0900

    配列の宣言の仕方

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -4,16 +4,13 @@ class MyApp
 {
     static void Main()
     {
-        //for文
-        // break
-        // 結果は「0 1 2 3 4」
-        for (var i = 0; i < 10; i++) {
-            if (i == 5)
-            {
-                break;
-            }
-            Console.WriteLine(i);
-        }
-
+        //配列の宣言
+        //scoresという変数に代入される値は数値なので、int[]とする
+        // int[3] とは、配列の中身が3つあるという意味。
+        int[] scores = new int[3];
+        //配列の中身それぞれに対して値を代入する
+        scores[0] = 10;
+        scores[1] = 30;
+        scores[2] = 20;
     }
 }
