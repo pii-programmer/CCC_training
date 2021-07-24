1938e26a78118fd640dadcdd02abd79b558eaed1
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-24 17:09:19 +0900

    配列の書き方3つ

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -5,12 +5,14 @@ class MyApp
     static void Main()
     {
         //配列の宣言
-        //scoresという変数に代入される値は数値なので、int[]とする
-        // int[3] とは、配列の中身が3つあるという意味。
-        int[] scores = new int[3];
-        //配列の中身それぞれに対して値を代入する
-        scores[0] = 10;
-        scores[1] = 30;
-        scores[2] = 20;
+        //配列の中身を{}に書くときはint[]内に3は不要
+        int[] scores = new int[] {10, 30, 20};
+
+        //より省略した書き方
+        int[] scores = { 10, 30, 20 };
+
+        //型推論での書き方
+        var scores = new [] { 10, 30, 20 };
+
     }
 }
