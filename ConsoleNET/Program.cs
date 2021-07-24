479b93215d26da73d6d3617b83c27f58cbd1aac0
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-24 17:12:34 +0900

    代入した値の上書き

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -5,14 +5,11 @@ class MyApp
     static void Main()
     {
         //配列の宣言
-        //配列の中身を{}に書くときはint[]内に3は不要
-        int[] scores = new int[] {10, 30, 20};
-
-        //より省略した書き方
-        int[] scores = { 10, 30, 20 };
-
-        //型推論での書き方
+        
         var scores = new [] { 10, 30, 20 };
 
+        //変数の中身の上書きもできる
+        scores[1] = 40;
+        Console.WriteLine(scores[1]);
     }
 }
