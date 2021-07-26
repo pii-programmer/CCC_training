c1f6f5f1601d1ba63845ce16900233631b808769
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-26 21:33:39 +0900

    foreach文

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -8,10 +8,16 @@ class MyApp
         
         var scores = new [] { 10, 30, 20 };
 
-        //for文：配列の 0~2 番を繰り返し表示する
+        // for文：配列の 0~2 番を繰り返し表示する
         for (int i = 0; i < scores.Length; i++)
         {
             Console.WriteLine(scores[i]);
+        }
+
+        //上記を foreach文でかくと↓ scoreという変数に scoresという配列の値を繰り返し代入する。
+        foreach(int score in scores)
+        {
+            Console.WriteLine(score);
         }
     }
 }
