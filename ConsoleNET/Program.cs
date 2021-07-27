e7f38c7299f8ca18b230fc31ce84e504748a2e2e
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-27 13:03:49 +0900

    メソッドを定義し呼び出してみる

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -2,22 +2,13 @@
 
 class MyApp
 {
+    static void SayHi()
+    {
+        Console.WriteLine("hi!");
+    }
     static void Main()
     {
-        //配列の宣言
-        
-        var scores = new [] { 10, 30, 20 };
-
-        // for文：配列の 0~2 番を繰り返し表示する
-        for (int i = 0; i < scores.Length; i++)
-        {
-            Console.WriteLine(scores[i]);
-        }
-
-        //上記を foreach文でかくと↓ scoreという変数に scoresという配列の値を繰り返し代入する。
-        foreach(int score in scores)
-        {
-            Console.WriteLine(score);
-        }
+        //メソッド＝処理をまとめる
+        SayHi();
     }
 }
