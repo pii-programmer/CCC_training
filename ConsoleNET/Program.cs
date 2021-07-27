b10abc5c31ffbd0da3b1ae573ae92363c0a47f02
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-07-27 18:12:07 +0900

    クラスの継承の仕方

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -1,34 +1,45 @@
 ﻿using System;
 
-// class
-// コンストラクタを使う
-// コンストラクタはインスタンスを作る時に必ず呼ばれるメソッドである
-// コンストラクタはクラスと同名のメソッドを定義してあげる
+// classの継承
+// User classを継承した AdminUser classを作る
 
-class User
+class User                                       //これが親クラス
 {
     public string name;
-    public User(string name)                     //これがコンストラクタ
+    public User(string name)                     //これが親クラスのコンストラクタ
     {
         // User classの外から値が渡ってきた（Tom）
         this.name = name;
     }
-    public User():this ("nobody"){               //オーバーロードの別の書き方。上のコンストラクタに"nobody"を渡してあげるのと同じ。
-    }
-    public void SayHi()
+    
+    public virtual void SayHi()                  //メソッド SayHi()を子クラスが上書きしているので virtualとする
     {
-        Console.Write($"Hi! {this.name}");
+        Console.Write($"Hi! {name}");            // thisを省略もできる
     }
 }
 
-// User classの外である MyApp classから、User classへ値を渡すこともできる
+class AdminUser: User                            //子クラスにコンストラクタは継承されない。その他は全て継承される。
+{
+    public AdminUser(string name) : base(name) { // base()は親クラスのコンストラクタのこと
+    }
+    public void SayHello()                       //子クラス内にも新しいメソッドを定義できる 
+    {
+        Console.Write($"Hello! {name}");
+    }
+    public override void SayHi()
+    {
+        Console.Write($"[admin] Hi! {name}");    //親クラスのメソッドを上書きする時 overrideとする。( [admin]は区別のために書いただけ）
+    }
+}
+
 class MyApp
 {
     static void Main()
     {
-        User tom = new User("Tom");  // User classのインスタンス tomを生成
-        tom.SayHi();                 // Hi! Tom
-        User user = new User();
-        user.SayHi();                // Hi! nobody
+        User tom = new User("Tom");              // User classのインスタンス tomを生成
+        tom.SayHi();                             // Hi! Tom
+        AdminUser bob = new AdminUser("Bob");    // AdminUser classのインスタンス bobを生成
+        bob.SayHi();                             // Hi! Bob
+        bob.SayHello();                          // Hello! Bob
     }
 }
