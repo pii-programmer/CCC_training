68bdb5aaeb8d3734dcd52fa26203619628229a15
pii.programmer <pii.programmer@gmail.com>
2021-08-01 21:13:18 +0900

    static修飾子の使い方

--- ConsoleNET/Program.cs
+++ ConsoleNET/Program.cs
@@ -2,25 +2,29 @@
 
 class MyApp
 {
-    //インデクサとは
-    //クラスに対してインスタンスを生成する時、配列と同じようにインデックスを作成できること。
+    // static修飾子は
+    // クラスのインスタンスを作らなくても、クラス外から、クラス内のフィールドやメソッドにアクセスできる。
 
-    class Team
+    class User
     {
-        private string[] members = new string[3];
-        public string this[int i]
+        private static int count = 0;   // これは countという staticフィールド
+        public User()                   // これはコンストラクタ
         {
-            get { return this.members[i]; }
-            set { this.members[i] = value; }
+            User.count++;
+        }
+        public static void GetCount()   // これは GetCount というstaticメソッド
+        {
+            Console.WriteLine($"# of instances: {count}");
         }
     }
 
     static void Main()
     {
-        Team giants = new Team();      // Team というクラスに giantsというインスタンスを生成
-        giants[0] = "taguchi";
-        giants[1] = "fkoji";
-        giants[2] = "dotinstall";
-        Console.WriteLine(giants[1]);  // 結果は fkoji
+        User.GetCount();               // 結果は 0 のはず
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();       // インスタンスを３つ作ったので
+        User.GetCount();               // 結果は 3 のはず
+
     }
 }
